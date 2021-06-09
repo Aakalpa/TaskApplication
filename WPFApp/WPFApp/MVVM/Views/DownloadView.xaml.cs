689f6c3bb92e97f1f0860d8e09a6190a021a48b4
Lumanti <lumantidangol05@gmail.com>
2021-06-09 09:13:41 +0545

    data loaded from api into download view;api integrated with download click and receive response; need to work on responsebody;

--- WPFApp/WPFApp/MVVM/Views/DownloadView.xaml.cs
+++ WPFApp/WPFApp/MVVM/Views/DownloadView.xaml.cs
@@ -1,6 +1,9 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -27,9 +30,57 @@ namespace WPFApp.MVVM.Views
             this.DataContext = new FileViewModel();
         }
 
-        private void Button_Click(object sender, RoutedEventArgs e)
+        public class UploadedFileList
         {
+            public string fileName { get; set; }
+            public string date { get; set; }
+            public string time { get; set; }
+        }
+        private async void BindUploadedFileLists()
+        {
+            HttpClient client = new HttpClient(new HttpClientHandler()
+            {
+                UseDefaultCredentials = true
+            });
 
+            client.BaseAddress = new Uri("https://localhost:44384/");
+            //client.DefaultRequestHeaders.Add("appkey", "myapp_key");
+            client.DefaultRequestHeaders.Accept.Add(
+               new MediaTypeWithQualityHeaderValue("application/json"));
+
+            HttpResponseMessage response = await client.GetAsync("api/FileUpload/GetFilesList/Images");
+            if (response.IsSuccessStatusCode)
+            {
+                string responseBody = await response.Content.ReadAsStringAsync();
+                System.Diagnostics.Debug.WriteLine(responseBody);
+                List<UploadedFileList> UploadedFiles = JsonConvert.DeserializeObject<List<UploadedFileList>>(responseBody);
+                System.Diagnostics.Debug.WriteLine(UploadedFiles);
+                UploadedFilesGrid.ItemsSource = (System.Collections.IEnumerable)UploadedFiles;
+            }
+            else
+            {
+                MessageBox.Show("Error Code" + response.StatusCode + " : Message - " + response.ReasonPhrase);
+            }
+        }
+
+        private void View_Loaded(object sender, RoutedEventArgs e)
+        {
+            BindUploadedFileLists();
+        }
+
+        private async void Download_Click(object sender, MouseButtonEventArgs e)
+        {
+            HttpClient client = new HttpClient(new HttpClientHandler()
+            {
+                UseDefaultCredentials = true
+            });
+            HttpResponseMessage response = await client.GetAsync("https://localhost:44384/api/FileUpload/DownloadFile/test/docs");
+            System.Diagnostics.Debug.WriteLine(response);
+            if (response.IsSuccessStatusCode)
+            {
+                string responseBody = await response.Content.ReadAsStringAsync();
+                System.Diagnostics.Debug.WriteLine(responseBody);
+            }
         }
     }
 }
